eeebe712cddb4525256266470a43d02d4d79c88b
muffin-button <36340578+muffin-button@users.noreply.github.com>
2020-05-26 00:17:15 -0400

    added Create and Update endpoints

--- src/index.js
+++ src/index.js
@@ -43,17 +43,30 @@ const collection = [
 ];
 
 
-app.use((req, res, next) => {
-    req.on('end', () => {
-        console.log(`Request: [${req.method}] URL: [${req.originalUrl}] Response: [${res.statusCode}] `);
-    });
-    next();
-});
+// app.use("*", (req, res, next) => {
+//     req.on('end', () => {
+//         console.log(`Request: [${req.method}] URL: [${req.originalUrl}] Response: [${res.statusCode}] `);
+//     });
+//     next();
+// });
 
 
 // Cat Breeds
 // Create
-//app.post('/');
+app.post('/', (req, res) => {
+    // Get cat breed details
+    const dto = req.body;
+    const newRecord = {
+        id: uuid(),
+        name: dto.name !== undefined ? dto.name + '' : '',
+        desc: dto.desc !== undefined ? dto.desc + '' : '',
+        items: []
+    }
+    // Add to list
+    collection.push(newRecord);
+    // Response
+    res.json(newRecord);
+});
 
 // Retrieve
 app.get('/', (req, res) => {
@@ -77,8 +90,20 @@ app.get('/:breedId', (req, res) => {
         res.json(result);
     }
 });
+
 // Update
-// app.put('/:breedId');
+app.put('/:breedId', (req, res) => {
+    // Get cat breed details
+    const dto = req.body;
+    const record = collection.find((x) => {
+        return x.id === req.params.breedId;
+    });
+    record.name = dto.name !== undefined ? dto.name + '' : '';
+    record.desc = dto.desc !== undefined ? dto.desc + '' : '';
+    // Response
+    res.json(record);
+});
+
 // Delete
 // app.delete('/:breedId');
 
